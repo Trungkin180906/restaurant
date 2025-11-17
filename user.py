774f2d9babdad3c74d9177dc32c28de4855dac16
Nguyễn Trung Kiên <nguyentrungkienbt2023@gmail.com>
2025-11-17 08:12:59 +0700

    Update user.py

--- user.py
+++ user.py
@@ -1,20 +1,27 @@
 class User:
-    def __init__(self, name, phone, email, password, role="customer"):
-        self.name=name
-        self.phone=phone
-        self.email=email
-        self.password=password
-        self.role=role
-#super() Cho phép tham chiếu đến Parent Class
-class Customer(User):
-    def __init__(self, name, phone, email, password):
-        super().__init__(name, phone, email, password, "customer")
-    
-class Staff(User):
-    def __init__(self, name, phone, birthday, email, password):
-        super().__init__(name, phone, birthday, email, password, "Staff")
+    def __init__(self, name, phone, email, password, role="customer", gender="khác"):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.password = password
+        self.role = role
+        self.gender = gender
 
-class Admin(User):
-     def __init__(self, name, phone, email, password):
-        super().__init__(name, phone, email, password, "Admin")
+class Customer(User):
+    def __init__(self, name, phone, email, password, gender="khác"):
+        super().__init__(name, phone, email, password, "customer", gender)
+
+class Staff(User):
+    def __init__(self, name, phone, email, password, gender="khác"):
+        super().__init__(name, phone, email, password, "staff", gender)
+
+class Admin:#admin ko kế thừa từ user nên ko dùng supper()
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+        self.role = "admin"
+
+
         
+
