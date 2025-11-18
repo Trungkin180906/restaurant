de116ac607668834fb5ad7b3143ed3532f00b552
Nguyễn Trung Kiên <nguyentrungkienbt2023@gmail.com>
2025-11-18 16:27:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# main.py
 from acount import register, login, forget_password
 from customer import customer_menu
 from staff import staff_menu
@@ -10,35 +11,35 @@ def main():
         print("2. Đăng nhập")
         print("3. Quên mật khẩu")
         print("0. Thoát")
-        choice = input("Chọn: ").strip()
+        choice = input("Chọn(0-3): ").strip()
 
-        if choice == "1":
+        if choice=="1":
             # Đăng ký
-            user = register()
+            user=register()
             if user:
                 print(f"Đăng ký thành công: {user.name} ({user.role})")
-        elif choice == "2":
+        elif choice=="2":
             # Đăng nhập
-            user = login()
+            user=login()
             if user:
                 print(f"Đăng nhập thành công: {user.name} ({user.role})")
                 # Phân quyền menu theo vai trò
-                if user.role == "customer":
+                if user.role=="customer":
                     customer_menu(user)
-                elif user.role == "staff":
+                elif user.role=="staff":
                     staff_menu(user)
-                elif user.role == "admin":
+                elif user.role=="admin":
                     admin_menu(user)
             else:
                 print("Sai email hoặc mật khẩu!")
-        elif choice == "3":
+        elif choice=="3":
             # Quên mật khẩu
             forget_password()
-        elif choice == "0":
+        elif choice=="0":
             print("Thoát hệ thống")
             break
         else:
-            print("Lựa chọn không hợp lệ! Vui lòng chọn lại.")
+            print("Lựa chọn không hợp lệ! Vui lòng chọn lại")
 
-if __name__ == "__main__":
+if __name__=="__main__":
     main()
